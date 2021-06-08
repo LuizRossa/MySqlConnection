ba9b27be0b6a4e9c6dda9c2fbeb602401d1cd93f
Luiz Rossa <Luiz@LUIZ-DESKTOP>
2021-06-07 21:31:25 -0300

    Add project files.

---
+++ MySqlConnection/Models/Contexto.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MySqlConnection.Models
+{
+    public class Contexto : DbContext
+    {
+        public Contexto(DbContextOptions<Contexto> options) : base(options)
+        {
+            Database.EnsureCreated();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseMySql(
+                "server=127.0.0.1;user id=root; password=@root987; persistsecurityinfo=True;database=gestaocaixa",
+                new MySqlServerVersion(new Version(8, 0, 11))
+            );
+        }
+
+        public DbSet<Usuarios> Usuario { get; set; }
+    }
+}
---
+++ MySqlConnection/Models/Usuarios.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MySqlConnection.Models
+{
+    public class Usuarios
+    {
+        [Display(Name = "idUsuarios")]
+        [Column("idUsuarios")]
+        public int id { get; set; }
+
+        [Display(Name = "nome")]
+        [Column("nome")]
+        public string nome { get; set; }
+
+        [Display(Name = "sobrenome")]
+        [Column("sobrenome")]
+        public string sobrenome { get; set; }
+
+        [Display(Name = "email")]
+        [Column("email")]
+        public string email { get; set; }
+
+        [Display(Name = "senha")]
+        [Column("senha")]
+        public string senha { get; set; }
+    }
+}
